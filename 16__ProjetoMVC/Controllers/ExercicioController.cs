f2f704d2de46ee8c03a3b37cb94d71be85edc2bd
Rafaela Lima! <limaderafaela018@gmail.com>
2023-09-28 07:20:41 -0300

    atualização

---
+++ 16__ProjetoMVC/Controllers/ExercicioController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace _16__ProjetoMVC.Controllers
+{
+
+    public class ExercicioController : Controller
+    {
+        private readonly ILogger<ExercicioController> _logger;
+
+        public ExercicioController(ILogger<ExercicioController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
---
+++ 16__ProjetoMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _16__ProjetoMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
